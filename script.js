f07f809249e28eccb0396f14c40f514fab3fa39f
Rashmy Shiraj <rashmyshiraj1@gmail.com>
2026-01-13 23:43:19 +0530

    added functionality to the task manager

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("Task manager app is loaded.")
+const taskInput = document.getElementById("task-input")
+const addTaskBtn = document.getElementById("task-input-button")
+const taskList = document.getElementById("task-list")
+
+function addTask(){
+    const taskText = taskInput.value.trim();
+
+    if(taskText == ""){
+        alert("Please enter a task");
+        return;
+    }   
+
+
+
+const li =document.createElement("li");
+li.textContent=taskText
+
+taskList.appendChild(li);
+
+taskInput.value = "";
+}
+
+addTaskBtn.addEventListener("click", addTask)
